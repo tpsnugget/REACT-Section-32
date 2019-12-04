17d0ecfed86ec58a4f77aa3b99af55a82b224b40
Michael Giebner <mikegiebner@gmail.com>
2019-12-04 17:10:51 -0500

    C5:32:250 complete, first use of useEffect

--- first-hooks-demo/src/App.js
+++ first-hooks-demo/src/App.js
@@ -3,16 +3,18 @@ import Toggler from "./Toggler"
 import SimpleFormClass from "./SimpleFormClass"
 import SimpleFormHooks from "./SimpleFormHooks"
 import SimpleFormInputHooks from "./SimpleFormInputHooks"
+import Clicker from "./Clicker"
 import './App.css';
 
 
 function App() {
   return (
     <div className="App">
-      <Toggler />
+      {/* <Toggler />
       <SimpleFormClass />
       <SimpleFormHooks />
-      <SimpleFormInputHooks />
+      <SimpleFormInputHooks /> */}
+      <Clicker />
     </div>
   );
 }
